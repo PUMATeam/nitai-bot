e54aae8ea8f0b0a27156bf3908986517e7e4be64
tde3000 <tde3000@gmail.com>
2016-07-16 16:05:26 +0300

    files are deleted after sending

--- src/controllers/PicController.js
+++ src/controllers/PicController.js
@@ -10,34 +10,23 @@ var twitter = new twitter_api.client(creds);
 class PicController extends NitaiBaseController {
     handle($){
         if ($._message._photo != null ){
-/*            console.log(twitter);
-            var params = {
-                status: "john " + $._message._photo[$._message._photo.length - 1]._fileId,
-            }
-            twitter_api.sendTweet(params, twitter,
-             function(reply){
-                console.log(reply);
-                $.sendMessage(reply);
-                $.sendMessage('תעשו לייק ;)');
-            });
-            
-   
-            exit;*/
             var img = api.getFile($._message._photo[$._message._photo.length - 1]._fileId);
             img.then(function(value) {
-                var file = fs.createWriteStream("file.jpg");
+                var filename = $._message._photo[$._message._photo.length - 1]._fileId + ".jpg";
+                var file = fs.createWriteStream(filename);
                 console.log(`https://api.telegram.org/file/bot${creds.key}/${value._filePath}`)
                 var request = https.get(`https://api.telegram.org/file/bot${creds.key}/${value._filePath}` ,
                               function(response) {
                               response.pipe(file);                          
                               }).on('close', function() {
-                                var data = fs.readFileSync('file.jpg');
+                                var data = fs.readFileSync(filename);
                                 console.log("uploading");
                                 console.log(data);
-                // upload media
-                               twitter_api.uploadMedia('file.jpg',twitter,function(media){
+                                // upload media
+                               twitter_api.uploadMedia(filename,twitter,function(media){
                                 // send media
-                               
+                                    console.log('deleting file');
+                                    fs.unlinkSync(filename);
                                     console.log(media);
                                     var text = "i'm eating";
                                     if ($._message._caption != null){
