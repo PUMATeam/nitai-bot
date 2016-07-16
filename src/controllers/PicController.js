b26d6dcaeabd2db9eaef2bea0153a6d02d1db90c
tde3000 <tde3000@gmail.com>
2016-07-16 18:23:08 +0300

    moved stuff, it's working

--- src/controllers/PicController.js
+++ src/controllers/PicController.js
@@ -1,55 +1,59 @@
-const api  = require('../bot.js').api;
-var https = require('https');
-var fs = require('fs');
+const Api  = require('../bot.js').api;
+const Https = require('https');
+const Fs = require('fs');
 const NitaiBaseController = require('./NitaiBaseController.js');
-var creds = require('./../../creds.js');
-var twitterApi = require('../twitter.js');
-var twitter = new twitterApi.client(creds);
+const Creds = require('./../../creds.js');
+const TwitterApi = require('../twitter.js');
+const Twitter = new TwitterApi.client(Creds);
+let file;
 
+var downloadTelegramFile = function(filePath, fileId, callback){
+    const Address =`https://api.telegram.org/file/bot${Creds.key}/${filePath}`;
+    file = Fs.createWriteStream(fileId);
+    Https.get(Address,callback);
+}
 
 class PicController extends NitaiBaseController {
     handle($){
         if ($._message._photo != null ){
-            var img = api.getFile($._message._photo[$._message._photo.length - 1]._fileId);
-            img.then(function(value) {
-                var filename = $._message._photo[$._message._photo.length - 1]._fileId + ".jpg";
-                var file = fs.createWriteStream(filename);
-                console.log(`https://api.telegram.org/file/bot${creds.key}/${value._filePath}`)
-                var request = https.get(`https://api.telegram.org/file/bot${creds.key}/${value._filePath}` ,
-                              function(response) {
-                              response.pipe(file);                          
-                              }).on('close', function() {
-                                var data = fs.readFileSync(filename);
-                                console.log("uploading");
-                                console.log(data);
-                                // upload media
-                               twitterApi.uploadMedia(filename,twitter,function(media){
-                                // send media
-                                    console.log('deleting file');
-                                    fs.unlinkSync(filename);
-                                    console.log(media);
-                                    var text = "i'm eating";
-                                    if ($._message._caption != null){
-                                        text = $._message._caption;
-                                    }
-                                    var params = {
-                                        status: text,
-                                        media_ids: media.media_id_string
-                                    }
-                                    console.log("sending status");
-                                    twitterApi.sendTweet(params, twitter,
-                                    function(reply){
-                                        console.log(reply);
-                                        $.sendMessage(reply);
-                                        $.sendMessage('תעשו לייק ;)');
-                                    });
+            const FileId = $._message._photo[$._message._photo.length - 1]._fileId;
+            Api.getFile(FileId)
+            .then( function(value){
+                downloadTelegramFile(value._filePath, FileId, 
+                function(response) {
+                    response.pipe(file)                         
+                    .on('close', function() {
+                    var data = Fs.readFileSync(FileId);
+                    console.log("uploading");
+                    console.log(data);
+                    // upload media
+                    TwitterApi.uploadMedia(FileId,Twitter,function(media){
+                    // send media
+                        console.log('deleting File');
+                        Fs.unlinkSync(FileId);
+                        console.log(media);
+                        var text = "i'm eating";
+                        if ($._message._caption != null){
+                            text = $._message._caption;
+                        }
+                        var params = {
+                            status: text,
+                            media_ids: media.media_id_string
+                        }
+                        console.log("sending status");
+                        TwitterApi.sendTweet(params, Twitter,
+                        function(reply){
+                            console.log(reply);
+                            $.sendMessage(reply);
+                            $.sendMessage('תעשו לייק ;)');
+                        });
 
-                                });
-                              });
-                
-            });            
+                    });
+                })})
+                })
+            };        
         }
     }
-}
+
 
 module.exports = PicController;
