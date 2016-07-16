af535ef0218d93bbed1b13b5d9edc469bdcf44de
Benny Zlotnik <uber442@gmail.com>
2016-07-16 20:46:36 +0300

    Finish issue #3

--- src/controllers/TimeController.js
+++ src/controllers/TimeController.js
@@ -9,7 +9,7 @@ class TimeController extends NitaiBaseController {
     
     constructor() {
         super();
-        this.currentDocument = {};
+        this.started = false;
     }
 
     handleTime($) {
@@ -17,6 +17,7 @@ class TimeController extends NitaiBaseController {
         let date = $._update.message.date;
         let username = $._update.message._from._username
         this.currentDocument = {};
+        this.started = $.chatSession.food_started || false;
 
         switch (command) {
             case 'started': this.logStartingTime($, date, username);
@@ -26,37 +27,59 @@ class TimeController extends NitaiBaseController {
         }
     }
 
+
+
     logStartingTime($, date, username)  {
-        let startingTime = moment.unix(date).format('HH:mm:ss');
+        if (this.started) {
+            $.sendMessage(`@${username}, Nitai has already started eating`);
+        } else {
+            let startingTime = moment.unix(date).format('HH:mm:ss');
 
-        winston.log('info', `Started: logged at ${startingTime} by ${username}`);
-    
-        $.sendMessage(`Nitai has started eating at ${startingTime}. Logged by ${username}`);
-
-        this.currentDocument.starting_time = date;
-        this.currentDocument.user_id = username;
+            winston.log('info', `Started: logged at ${startingTime} by ${username}`);
         
-        database.insertDocument(this.currentDocument).then((id) => {
-            $.chatSession.lastId = id;
-        });
+            $.sendMessage(`Nitai has started eating at ${startingTime}. Logged by ${username}`);
+
+            this.currentDocument.starting_time = date;
+            this.currentDocument.user_id = username;
+            
+            database.insertDocument(this.currentDocument).then((id) => {
+                $.chatSession.lastId = id;
+            });
+            this.started = $.chatSession.food_started = true;   
+     }
     } 
 
     logFinishingTime($, date, username) {
-        let endingTime = moment.unix(date).format('HH:mm:ss');
+        if (!this.started) {
+            $.sendMessage(`@${username}, Nitai has not started eating yet`)
+        } else {
+            let endingTime = moment.unix(date).format('HH:mm:ss');
 
-        winston.log('info', `Finished: logged at ${endingTime} by ${username}`);
-        $.sendMessage(`Nitai has finished eating at ${endingTime}. Logged by ${username}`);
-        
-        database.getRowById($.chatSession.lastId).then((result) => {
-            this.currentDocument = result;
-        }).catch((err) => {
-            winston.error(`Getting row failed: ${err}`);
-        });
+            winston.log('info', `Finished: logged at ${endingTime} by ${username}`);
+            $.sendMessage(`Nitai has finished eating at ${endingTime}. Logged by ${username}`);
+            
+            database.getRowById($.chatSession.lastId).then((result) => {
+                this.currentDocument = result;
+            }).catch((err) => {
+                winston.error(`Getting row failed: ${err}`);
+            });
 
-        this.currentDocument.ending_time = date;
-        winston.log('info', `Last food_log id  ${$.chatSession.lastId}`);
+            this.currentDocument.ending_time = date;
+            winston.log('info', `Last food_log id  ${$.chatSession.lastId}`);
 
-        database.updateDocument(this.currentDocument, $.chatSession.lastId);
+            database.updateDocument(this.currentDocument, $.chatSession.lastId);
+            this.started = $.chatSession.food_started = false;
+            this.sendSummary($, this.currentDocument);
+        }
+    }
+
+    // TODO: extract time difference to a utility method
+    sendSummary($, document) {
+        let start = moment.unix(document.starting_time);
+        let end = moment.unix(document.ending_time);
+        let difference = end.diff(start);
+        let duration = moment.duration(difference, 'minutes').humanize();
+        $.sendMessage(`It took me ${duration} to eat!`);
     }
 
     get routes() {
