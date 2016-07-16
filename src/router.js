ea032a87e1f99ef4b644ede083eb2916b928a464
Benny Zlotnik <uber442@gmail.com>
2016-07-16 18:13:26 +0300

    Remove database declaration

--- src/router.js
+++ src/router.js
@@ -1,12 +1,8 @@
 const PingController = require('./controllers/PingController');
 const TimeController = require('./controllers/TimeController');
-const Database = require('./db/database');
 
-const config = require('./config')
 const router = require('./bot').router;
 const winston = require('winston');
 
-winston.log('info', `Connecting to database... ${config.database_url}`);
-
 router.when(['ping'], new PingController());
-router.when('/time :command', new TimeController(new Database(config.database_url, config.table_name)));
+router.when('/time :command', new TimeController());
