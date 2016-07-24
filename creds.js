15fa9ade96dc2192a1d81f5621c0fc15dbcab5dd
Benny Zlotnik <uber442@gmail.com>
2016-07-24 20:45:43 +0300

    Update creds

--- creds.js
+++ creds.js
@@ -1,8 +1,9 @@
-var api = {};
+let api = {};
 api.key = process.env.TELEGRAM_KEY;
 api.tw_consumer = process.env.TW_CONS;
 api.tw_consumer_secret = process.env.TW_CONS_SEC;
 api.tw_access = process.env.TW_ACC;
 api.tw_acc_secret = process.env.TW_ACC_SEC;
+api.github_token = process.env.GITHUB_API;
 
 module.exports = api;
